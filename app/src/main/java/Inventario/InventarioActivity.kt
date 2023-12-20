d7116011a3e7dbf9c70bf246db69491db6c67e9d
DogyMaster <lopo1231@hotmail.com>
2023-12-20 00:52:00 -0500

    se a creado un diseño preliminar para crear productos y agregarlos en la base datos, falta organizar el diseño y poner algunas reglas para poder ingresar dichos productos

--- app/src/main/java/Inventario/InventarioActivity.kt
+++ app/src/main/java/Inventario/InventarioActivity.kt
@@ -1,12 +1,117 @@
 package Inventario
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.mostery.wizbix.R
 
 class InventarioActivity : AppCompatActivity() {
+
+    private lateinit var categorias: MutableList<String>
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inventario)
+
+        // Obtener la lista de categorías de ConfiguracionActivity (puedes usar SharedPreferences o intent extras)
+        // Por ejemplo, si utilizas SharedPreferences:
+        val sharedPreferences = getSharedPreferences("Categorias", MODE_PRIVATE)
+        categorias =
+            sharedPreferences.getStringSet("categorias", HashSet<String>())?.toMutableList()
+                ?: mutableListOf()
+
+        // Inicializar Firebase
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.reference
+
+        // Resto de la lógica de tu actividad...
     }
+
+    fun mostrarFormularioCrearProducto(view: View) {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_crear_producto, null)
+
+        val nombreEditText = dialogView.findViewById<EditText>(R.id.editTextNombre)
+        val descripcionEditText = dialogView.findViewById<EditText>(R.id.editTextDescripcion)
+        val precioEditText = dialogView.findViewById<EditText>(R.id.editTextPrecio)
+        val codigoAlternoEditText = dialogView.findViewById<EditText>(R.id.editTextCodigoAlterno)
+        val categoriaSpinner = dialogView.findViewById<Spinner>(R.id.spinnerCategoria)
+
+        // Configurar el Spinner con las categorías disponibles
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categorias)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        categoriaSpinner.adapter = adapter
+
+        val dialogBuilder = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .setTitle("Crear Producto")
+            .setPositiveButton("Guardar") { dialog, _ ->
+                // Guardar el producto en la base de datos aquí
+                val nombre = nombreEditText.text.toString()
+                val descripcion = descripcionEditText.text.toString()
+                val precio = precioEditText.text.toString()
+                val codigoAlterno = codigoAlternoEditText.text.toString()
+                val categoriaSeleccionada = categoriaSpinner.selectedItem.toString()
+
+                // Aquí puedes guardar los datos en Firebase
+                guardarProductoEnFirebase(
+                    nombre,
+                    descripcion,
+                    precio,
+                    codigoAlterno,
+                    categoriaSeleccionada
+                )
+
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancelar") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        val dialog = dialogBuilder.create()
+        dialog.show()
+    }
+    private fun mostrarMensaje(mensaje: String) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun guardarProductoEnFirebase(
+        nombre: String,
+        descripcion: String,
+        precio: String,
+        codigoAlterno: String,
+        categoria: String
+    ) {
+        val productosReference =
+            databaseReference.child("productos") // Nombre del nodo "productos" en tu base de datos
+
+        val nuevoProducto = HashMap<String, Any>()
+        nuevoProducto["nombre"] = nombre
+        nuevoProducto["descripcion"] = descripcion
+        nuevoProducto["precio"] = precio
+        nuevoProducto["codigoAlterno"] = codigoAlterno
+        nuevoProducto["categoria"] = categoria
+
+        productosReference.push().setValue(nuevoProducto)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    mostrarMensaje("Producto guardado correctamente")
+                } else {
+                    mostrarMensaje("Error al guardar el producto")
+                    // Muestra el mensaje de error específico si lo proporciona Firebase
+                    task.exception?.message?.let { mostrarMensaje(it) }
+                }
+            }
+
+    }
+
 }
